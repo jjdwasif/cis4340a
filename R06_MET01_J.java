62578364f5211464ca3f32be0ba27f49827c0b32
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 12:11:50 -0500

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,3 +1,6 @@
+//Rule 06. Methods (MET)
+//MET01-J. Never use assertions to validate method arguments Given the non-compliant code below:
+
 public static int getAbsAdd(int x, int y) {
   assert x != Integer.MIN_VALUE;
   assert y != Integer.MIN_VALUE;
