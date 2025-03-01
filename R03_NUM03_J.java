4626daa257e3eaa2331d1f9964fdf76083db3e30
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 12:12:52 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,6 @@
+//Rule 03. Numeric Types and Operations (NUM)
+//NUM03-J. Use integer types that can fully represent the possible range of unsigned data Given the non-compliant code below:
+
 public static int getInteger(DataInputStream is) throws IOException {
   return is.readInt(); 
 }
