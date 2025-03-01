b7dbc12c77657aada159079729ad6191276b0fe9
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 12:04:04 -0500

    Create R07_ERR05_J.java

---
+++ R07_ERR05_J.java
@@ -0,0 +1,42 @@
+public class Operation {
+  public static void doOperation(String some_file) {
+    // ... Code to check or set character encoding ...
+    try {
+      BufferedReader reader =
+          new BufferedReader(new FileReader(some_file));
+      try {
+        // Do operations
+      } finally {
+        reader.close();
+        // ... Other cleanup code ...
+      }
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
+
+
+
+// Compliant
+public class Operation {
+  public static void doOperation(String some_file) {
+    // ... Code to check or set character encoding ...
+    try {
+      BufferedReader reader =
+          new BufferedReader(new FileReader(some_file));
+      try {
+        // Do operations
+      } finally {
+        try {
+          reader.close();
+        } catch (IOException ie) {
+          // Forward to handler
+        }
+        // ... Other cleanup code ...
+      }
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
