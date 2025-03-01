01699ea574453df2a297df937bbb09945c41926c
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 12:17:03 -0500

    Update R07_ERR05_J.java

--- R07_ERR05_J.java
+++ R07_ERR05_J.java
@@ -1,3 +1,6 @@
+// Rule 07. Exceptional Behavior (ERR)
+// ERR05-J. Do not let checked Exceptions escape from a finally block 
+
 public class Operation {
   public static void doOperation(String some_file) {
     // ... Code to check or set character encoding ...
