d8ccb718596e38b805d240ea65e52d15d4fb07c1
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 11:58:15 -0500

    Create R08_VNA03_J.java

---
+++ R08_VNA03_J.java
@@ -0,0 +1,40 @@
+final class Adder {
+  private final AtomicReference<BigInteger> first;
+  private final AtomicReference<BigInteger> second;
+ 
+  public Adder(BigInteger f, BigInteger s) {
+    first  = new AtomicReference<BigInteger>(f);
+    second = new AtomicReference<BigInteger>(s);
+  }
+ 
+  public void update(BigInteger f, BigInteger s) { // Unsafe
+    first.set(f);
+    second.set(s);
+  }
+ 
+  public BigInteger add() { // Unsafe
+    return first.get().add(second.get());
+  }
+}
+// Compliant
+final class Adder {
+  // ...
+  private final AtomicReference<BigInteger> first;
+  private final AtomicReference<BigInteger> second;
+ 
+  public Adder(BigInteger f, BigInteger s) {
+    first  = new AtomicReference<BigInteger>(f);
+    second = new AtomicReference<BigInteger>(s);
+  }
+ 
+ 
+ 
+  public synchronized void update(BigInteger f, BigInteger s){
+    first.set(f);
+    second.set(s);
+  }
+ 
+  public synchronized BigInteger add() {
+    return first.get().add(second.get());
+  }
+}
