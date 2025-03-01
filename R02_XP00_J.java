fb9755755bf1ab5375898746cc8ad91da6cfedee
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 11:31:28 -0500

    Create R02_XP00_J.java

---
+++ R02_XP00_J.java
@@ -0,0 +1,12 @@
+public void deleteFile() {
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
+}
+public void deleteFile() {
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+    }
+}
