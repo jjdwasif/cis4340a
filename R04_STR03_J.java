a102986071c7791623c493c3b23c6cb769a08e05
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 12:12:22 -0500

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,3 +1,6 @@
+//Rule 04. Characters and Strings (STR)
+//STR03-J. Do not encode noncharacter data as a string Given the non-compliant code below:
+
 BigInteger x = new BigInteger("530500452766");
 byte[] byteArray = x.toByteArray();
 String s = new String(byteArray);
