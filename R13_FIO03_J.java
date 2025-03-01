151b8ec1379dcf716cd1258b4cfb9087b24dc57f
jjdwasif <jjdwasif@hotmail.com>
2025-03-01 11:59:58 -0500

    Create R13_FIO03_J.java

---
+++ R13_FIO03_J.java
@@ -0,0 +1,44 @@
+class TempFile {
+  public static void main(String[] args) throws IOException{
+    File f = File.createTempFile("tempnam",".tmp");
+    FileOutputStream fop = null;
+    try {
+      fop = new FileOutputStream(f);
+      String str = "Data";
+      fop.write(str.getBytes());
+      fop.flush();
+    } finally {
+      // Stream/file still open; file will
+      // not be deleted on Windows systems
+      f.deleteOnExit(); // Delete the file when the JVM terminates
+ 
+      if (fop != null) {
+        try {
+          fop.close();
+        } catch (IOException x) {
+          // Handle error
+        }
+      }
+    }
+  }
+}
+// Compliant
+class TempFile {
+  public static void main(String[] args) {
+    Path tempFile = null;
+    try {
+      tempFile = Files.createTempFile("tempnam", ".tmp");
+      try (BufferedWriter writer =
+          Files.newBufferedWriter(tempFile, Charset.forName("UTF8"),
+                                  StandardOpenOption.DELETE_ON_CLOSE)) {
+        // Write to file
+      }
+      System.out.println("Temporary file write done, file erased");
+    } catch (FileAlreadyExistsException x) {
+      System.err.println("File exists: " + tempFile);
+    } catch (IOException x) {
+      // Some other sort of failure, such as permissions.
+      System.err.println("Error creating temporary file: " + x);
+    }
+  }
+}
